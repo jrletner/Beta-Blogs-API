f1b96116788cfe86ded4c54cef243eef31c032e6
Joe Letner <jrletner@gmail.com>
2023-04-01 16:13:23 -0500

    add seed data & migration to add username to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_19_225512) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_01_203319) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -77,6 +77,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_19_225512) do
     t.datetime "invitation_expiration", precision: nil
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# creating users
+(0..10).each do
+  User.create(email: Faker::Internet.email, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, phone: "1", password: "password", username: Faker::Internet.username)
+end
+
+# creating a list of categories
+Category.create(name: "JavaScript")
+Category.create(name: "HTML")
+Category.create(name: "CSS")
+Category.create(name: "Ruby on Rails")
+Category.create(name: "Angular")
+
+# create blogs
+
+# - a category
+# - a user
+# - blog info
+
+(0..100).each do |i|
+  faker_title = Faker::Hacker.noun.capitalize + " " + Faker::Hacker.verb.capitalize
+  fake_sub_title = Faker::Lorem.sentence
+  num_paragraphs = rand(1..5)
+  paragraphs = ""
+  num_paragraphs.times do
+    paragraphs += Faker::Lorem.paragraph(sentence_count: 3) + "\n\n"
+  end
+
+  blog = Blog.new(user_id: rand(0..9), title: faker_title, sub_title: fake_sub_title, content: paragraphs, image_path: "https://www.searchenginejournal.com/wp-content/uploads/2022/06/image-search-1600-x-840-px-62c6dc4ff1eee-sej.png")
+
+  blog.categories = Category.all.sample(rand(0..6))
+  blog.save
+end
